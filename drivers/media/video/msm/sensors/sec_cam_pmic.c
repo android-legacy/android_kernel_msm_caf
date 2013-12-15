f86bbbc4c8efb1cc4d8177fa1535aed2c70e0b82
erikcas <erikcas1972@gmail.com>
2013-12-15 15:16:24 +0100

    Revert "trial and error #2"
    This reverts commit 0abfafe696f3537a3baef89ab9f9cbcd6883611b.

--- drivers/media/video/msm/sensors/sec_cam_pmic.c
+++ drivers/media/video/msm/sensors/sec_cam_pmic.c
@@ -11,6 +11,7 @@
 #include <linux/io.h>
 #include <mach/board.h>
 #include <mach/msm_iomap.h>
+#include <mach/gpio_jena.h>
 
 #include <linux/regulator/consumer.h>
 #include <mach/vreg.h>
@@ -18,7 +19,7 @@
 
 #include "sec_cam_pmic.h"
 
-
+#define GPIO_3M_CAM_A_EN        128
 
 
 #if defined(CONFIG_S5K5CCGX)//Power on/off functions for S5KCCGX sensor
@@ -108,39 +109,39 @@ void cam_ldo_power_on_1(void)
 	int ret;
 	printk("#### cam_ldo_power_on_1 ####\n");
 
-	ret = gpio_request(ARUBA_CAM_STBY, "aruba_stby");
-	ret = gpio_request(ARUBA_CAM_RESET, "aruba_reset");
-	ret = gpio_request(ARUBA_CAM_A_EN, "aruba_a_en");
+	ret = gpio_request(GPIO_3M_CAM_STBY, "jena_stby");
+	ret = gpio_request(GPIO_3M_CAM_RESET, "jena_reset");
+	ret = gpio_request(GPIO_3M_CAM_A_EN, "jena_a_en");
 #ifdef CONFIG_S5K4ECGX
-	ret = gpio_request(ARUBA_CAM_C_EN, "aruba_c_en");
-	ret = gpio_request(ARUBA_CAM_AF_EN, "aruba_af_en");
+	ret = gpio_request(GPIO_3M_CAM_C_EN, "jena_c_en");
+	ret = gpio_request(GPIO_3M_CAM_AF_EN, "jena_af_en");
 #endif
-	gpio_direction_output(ARUBA_CAM_STBY, 0);
-	gpio_direction_output(ARUBA_CAM_RESET, 0);
-	gpio_direction_output(ARUBA_CAM_A_EN, 0);
+	gpio_direction_output(GPIO_3M_CAM_STBY, 0);
+	gpio_direction_output(GPIO_3M_CAM_RESET, 0);
+	gpio_direction_output(GPIO_3M_CAM_A_EN, 0);
 #ifdef CONFIG_S5K4ECGX
-	gpio_direction_output(ARUBA_CAM_C_EN, 0);
-	gpio_direction_output(ARUBA_CAM_AF_EN, 0);
+	gpio_direction_output(GPIO_3M_CAM_C_EN, 0);
+	gpio_direction_output(GPIO_3M_CAM_AF_EN, 0);
 #endif
 
-	gpio_set_value_cansleep(ARUBA_CAM_STBY, 0);		//STBYN :: MSM_GPIO 96
-	gpio_set_value_cansleep(ARUBA_CAM_RESET, 0);	//RSTN :: MSM_GPIO 85
-	gpio_set_value_cansleep(ARUBA_CAM_A_EN, 0);
+	gpio_set_value_cansleep(GPIO_3M_CAM_STBY, 0);		//STBYN :: MSM_GPIO 96
+	gpio_set_value_cansleep(GPIO_3M_CAM_RESET, 0);	//RSTN :: MSM_GPIO 85
+	gpio_set_value_cansleep(GPIO_3M_CAM_A_EN, 0);
 #ifdef CONFIG_S5K4ECGX
-	gpio_set_value_cansleep(ARUBA_CAM_C_EN, 0);
+	gpio_set_value_cansleep(GPIO_3M_CAM_C_EN, 0);
 	msleep(1);
 
-//	printk("ARUBA_CAM_C_EN = %d , CAM_AF_EN = %d \n", gpio_get_value(ARUBA_CAM_C_EN), gpio_get_value(ARUBA_CAM_AF_EN));
-	gpio_set_value(ARUBA_CAM_C_EN, 1);
-	gpio_set_value(ARUBA_CAM_AF_EN, 1);
+//	printk("GPIO_3M_CAM_C_EN = %d , CAM_AF_EN = %d \n", gpio_get_value(GPIO_3M_CAM_C_EN), gpio_get_value(GPIO_3M_CAM_AF_EN));
+	gpio_set_value(GPIO_3M_CAM_C_EN, 1);
+	gpio_set_value(GPIO_3M_CAM_AF_EN, 1);
 	msleep(1);
-//	printk("ARUBA_CAM_C_EN = %d , CAM_AF_EN = %d \n", gpio_get_value(ARUBA_CAM_C_EN), gpio_get_value(ARUBA_CAM_AF_EN));
+//	printk("GPIO_3M_CAM_C_EN = %d , CAM_AF_EN = %d \n", gpio_get_value(GPIO_3M_CAM_C_EN), gpio_get_value(GPIO_3M_CAM_AF_EN));
 #endif
 
-//	printk("ARUBA_CAM_A_EN = %d\n", gpio_get_value(ARUBA_CAM_A_EN));
+//	printk("GPIO_3M_CAM_A_EN = %d\n", gpio_get_value(GPIO_3M_CAM_A_EN));
 
-	gpio_set_value(ARUBA_CAM_A_EN, 1);
-//	printk("ARUBA_CAM_A_EN = %d\n", gpio_get_value(ARUBA_CAM_A_EN));
+	gpio_set_value(GPIO_3M_CAM_A_EN, 1);
+//	printk("GPIO_3M_CAM_A_EN = %d\n", gpio_get_value(GPIO_3M_CAM_A_EN));
 
 
 
@@ -152,15 +153,15 @@ void cam_ldo_power_on_2(void)
 	int ret;
 	printk("#### cam_ldo_power_on_2 ####\n");
 
-//	printk("ARUBA_CAM_STBY = %d\n", gpio_get_value(ARUBA_CAM_STBY));
+//	printk("GPIO_3M_CAM_STBY = %d\n", gpio_get_value(GPIO_3M_CAM_STBY));
 	msleep(1);
-	gpio_set_value_cansleep(ARUBA_CAM_STBY, 1);		//STBYN :: MSM_GPIO 96
+	gpio_set_value_cansleep(GPIO_3M_CAM_STBY, 1);		//STBYN :: MSM_GPIO 96
 	msleep(1);
-//	printk("ARUBA_CAM_STBY = %d\n", gpio_get_value(ARUBA_CAM_STBY));
-//	printk("ARUBA_CAM_RESET = %d\n", gpio_get_value(ARUBA_CAM_RESET));
-	gpio_set_value_cansleep(ARUBA_CAM_RESET, 1);	//RSTN :: MSM_GPIO 85
+//	printk("GPIO_3M_CAM_STBY = %d\n", gpio_get_value(GPIO_3M_CAM_STBY));
+//	printk("GPIO_3M_CAM_RESET = %d\n", gpio_get_value(GPIO_3M_CAM_RESET));
+	gpio_set_value_cansleep(GPIO_3M_CAM_RESET, 1);	//RSTN :: MSM_GPIO 85
 	msleep(1);
-//	printk("ARUBA_CAM_RESET = %d\n", gpio_get_value(ARUBA_CAM_RESET));	
+//	printk("GPIO_3M_CAM_RESET = %d\n", gpio_get_value(GPIO_3M_CAM_RESET));	
 }
 
 
@@ -170,27 +171,27 @@ void cam_ldo_power_off_1(void)
 {
 	printk("#### cam_ldo_power_off_1 ####\n");
 
-	gpio_set_value_cansleep(ARUBA_CAM_RESET, 0);	//RSTN :: MSM_GPIO 85
+	gpio_set_value_cansleep(GPIO_3M_CAM_RESET, 0);	//RSTN :: MSM_GPIO 85
 
 }
 void cam_ldo_power_off_2(void)
 {
 	printk("#### cam_ldo_power_off_2 ####\n");
 	
-	gpio_set_value_cansleep(ARUBA_CAM_STBY, 0);		//STBYN :: MSM_GPIO 96
-	gpio_set_value_cansleep(ARUBA_CAM_A_EN, 0);
+	gpio_set_value_cansleep(GPIO_3M_CAM_STBY, 0);		//STBYN :: MSM_GPIO 96
+	gpio_set_value_cansleep(GPIO_3M_CAM_A_EN, 0);
 #ifdef CONFIG_S5K4ECGX
-	gpio_set_value_cansleep(ARUBA_CAM_C_EN, 0);
-	gpio_set_value_cansleep(ARUBA_CAM_AF_EN, 0);
+	gpio_set_value_cansleep(GPIO_3M_CAM_C_EN, 0);
+	gpio_set_value_cansleep(GPIO_3M_CAM_AF_EN, 0);
 #endif
 	msleep(10);
 #ifdef CONFIG_S5K4ECGX
-	gpio_free(ARUBA_CAM_C_EN);
-	gpio_free(ARUBA_CAM_AF_EN);
+	gpio_free(GPIO_3M_CAM_C_EN);
+	gpio_free(GPIO_3M_CAM_AF_EN);
 #endif
-	gpio_free(ARUBA_CAM_A_EN);
-	gpio_free(ARUBA_CAM_STBY);
-	gpio_free(ARUBA_CAM_RESET);
+	gpio_free(GPIO_3M_CAM_A_EN);
+	gpio_free(GPIO_3M_CAM_STBY);
+	gpio_free(GPIO_3M_CAM_RESET);
 }
 #endif
 
@@ -207,25 +208,25 @@ void cam_flash_main_on(int flashMode)
 		return;
 	}
 	
-	ret = gpio_request(ARUBA_CAM_FLASH_EN, "aruba_flash_en");
-	ret = gpio_request(ARUBA_CAM_FLASH_SET, "aruba_flash_set");
-	printk("ARUBA_CAM_FLASH_EN = %d\n", gpio_get_value(ARUBA_CAM_FLASH_EN));
-	printk("ARUBA_CAM_FLASH_SET = %d\n", gpio_get_value(ARUBA_CAM_FLASH_SET));
+	ret = gpio_request(GPIO_3M_CAM_FLASH_EN, "jena_flash_en");
+	ret = gpio_request(GPIO_3M_CAM_FLASH_SET, "jena_flash_set");
+	printk("GPIO_3M_CAM_FLASH_EN = %d\n", gpio_get_value(GPIO_3M_CAM_FLASH_EN));
+	printk("GPIO_3M_CAM_FLASH_SET = %d\n", gpio_get_value(GPIO_3M_CAM_FLASH_SET));
 	
-	gpio_set_value_cansleep(ARUBA_CAM_FLASH_SET, 0);
+	gpio_set_value_cansleep(GPIO_3M_CAM_FLASH_SET, 0);
 	int i;
 	for(i=0;i<15;i++)
 	{
-		gpio_set_value_cansleep(ARUBA_CAM_FLASH_EN, 1);
+		gpio_set_value_cansleep(GPIO_3M_CAM_FLASH_EN, 1);
 		mdelay(1);
-		gpio_set_value_cansleep(ARUBA_CAM_FLASH_EN, 0);
+		gpio_set_value_cansleep(GPIO_3M_CAM_FLASH_EN, 0);
 		printk("count %d \n",i);
 	}
-	gpio_set_value_cansleep(ARUBA_CAM_FLASH_EN, 1);
-	gpio_set_value_cansleep(ARUBA_CAM_FLASH_SET, 0);
+	gpio_set_value_cansleep(GPIO_3M_CAM_FLASH_EN, 1);
+	gpio_set_value_cansleep(GPIO_3M_CAM_FLASH_SET, 0);
 	msleep(10);
-	printk("ARUBA_CAM_FLASH_EN = %d\n", gpio_get_value(ARUBA_CAM_FLASH_EN));
-	printk("ARUBA_CAM_FLASH_SET = %d\n", gpio_get_value(ARUBA_CAM_FLASH_SET));
+	printk("GPIO_3M_CAM_FLASH_EN = %d\n", gpio_get_value(GPIO_3M_CAM_FLASH_EN));
+	printk("GPIO_3M_CAM_FLASH_SET = %d\n", gpio_get_value(GPIO_3M_CAM_FLASH_SET));
 
 	if (ret < 0) {
 		printk("cam_flash_main_on GPIO Request Fail !!! \n");
@@ -244,10 +245,10 @@ void cam_flash_torch_on(int flashMode)
 		return;
 	}
 
-	ret = gpio_request(ARUBA_CAM_FLASH_EN, "aruba_flash_en");
-	ret = gpio_request(ARUBA_CAM_FLASH_SET, "aruba_flash_set");
-	gpio_set_value_cansleep(ARUBA_CAM_FLASH_EN, 1);
-		gpio_set_value_cansleep(ARUBA_CAM_FLASH_SET, 0);
+	ret = gpio_request(GPIO_3M_CAM_FLASH_EN, "jena_flash_en");
+	ret = gpio_request(GPIO_3M_CAM_FLASH_SET, "jena_flash_set");
+	gpio_set_value_cansleep(GPIO_3M_CAM_FLASH_EN, 1);
+		gpio_set_value_cansleep(GPIO_3M_CAM_FLASH_SET, 0);
 
 	if (ret < 0) {
 		printk("cam_flash_main_on GPIO Request Fail !!! \n");
@@ -267,16 +268,16 @@ void cam_flash_off(int flashMode)
 	}
 
 	
-	ret = gpio_request(ARUBA_CAM_FLASH_EN, "aruba_flash_en");
-	ret = gpio_request(ARUBA_CAM_FLASH_SET, "aruba_flash_set");
-	gpio_set_value_cansleep(ARUBA_CAM_FLASH_EN, 0);
-	gpio_set_value_cansleep(ARUBA_CAM_FLASH_SET, 0);
+	ret = gpio_request(GPIO_3M_CAM_FLASH_EN, "jena_flash_en");
+	ret = gpio_request(GPIO_3M_CAM_FLASH_SET, "jena_flash_set");
+	gpio_set_value_cansleep(GPIO_3M_CAM_FLASH_EN, 0);
+	gpio_set_value_cansleep(GPIO_3M_CAM_FLASH_SET, 0);
 
 	if (ret < 0) {
 		printk("cam_flash_main_on GPIO Request Fail !!! \n");
 	}
-	gpio_free(ARUBA_CAM_FLASH_EN);
-	gpio_free(ARUBA_CAM_FLASH_SET);
+	gpio_free(GPIO_3M_CAM_FLASH_EN);
+	gpio_free(GPIO_3M_CAM_FLASH_SET);
 	}
 
 #endif
@@ -322,40 +323,40 @@ int32_t msm_sensor_power_up_baffin_duos(struct msm_sensor_ctrl_t *s_ctrl)
 	int ret;
 	printk("#### cam_ldo_power_on_1 ####\n");
 
-	ret = gpio_request(ARUBA_CAM_STBY, "baffin_stby");
-	ret = gpio_request(ARUBA_CAM_RESET, "baffin_reset");
-	ret = gpio_request(ARUBA_CAM_A_EN, "baffin_a_en");
-	ret = gpio_request(ARUBA_CAM_C_EN, "baffin_c_en");
-	ret = gpio_request(ARUBA_CAM_AF_EN, "baffin_af_en");
+	ret = gpio_request(GPIO_3M_CAM_STBY, "baffin_stby");
+	ret = gpio_request(GPIO_3M_CAM_RESET, "baffin_reset");
+	ret = gpio_request(GPIO_3M_CAM_A_EN, "baffin_a_en");
+	ret = gpio_request(GPIO_3M_CAM_C_EN, "baffin_c_en");
+	ret = gpio_request(GPIO_3M_CAM_AF_EN, "baffin_af_en");
 	ret = gpio_request(FRONT_CAM_STBY, "front_cam_stby");
 	ret = gpio_request(FRONT_CAM_RESET, "front_cam_reset");
 
-	gpio_direction_output(ARUBA_CAM_STBY, 0);
-	gpio_direction_output(ARUBA_CAM_RESET, 0);
-	gpio_direction_output(ARUBA_CAM_A_EN, 0);
-	gpio_direction_output(ARUBA_CAM_C_EN, 0);
-	gpio_direction_output(ARUBA_CAM_AF_EN, 0);
+	gpio_direction_output(GPIO_3M_CAM_STBY, 0);
+	gpio_direction_output(GPIO_3M_CAM_RESET, 0);
+	gpio_direction_output(GPIO_3M_CAM_A_EN, 0);
+	gpio_direction_output(GPIO_3M_CAM_C_EN, 0);
+	gpio_direction_output(GPIO_3M_CAM_AF_EN, 0);
 	gpio_direction_output(FRONT_CAM_STBY, 0);
 	gpio_direction_output(FRONT_CAM_RESET, 0);
 
 	
-	gpio_set_value_cansleep(ARUBA_CAM_STBY, 0); 	//STBYN :: MSM_GPIO 96
-	gpio_set_value_cansleep(ARUBA_CAM_RESET, 0);	//RSTN :: MSM_GPIO 85
-	gpio_set_value_cansleep(ARUBA_CAM_A_EN, 0);
-	gpio_set_value_cansleep(ARUBA_CAM_C_EN, 0);
+	gpio_set_value_cansleep(GPIO_3M_CAM_STBY, 0); 	//STBYN :: MSM_GPIO 96
+	gpio_set_value_cansleep(GPIO_3M_CAM_RESET, 0);	//RSTN :: MSM_GPIO 85
+	gpio_set_value_cansleep(GPIO_3M_CAM_A_EN, 0);
+	gpio_set_value_cansleep(GPIO_3M_CAM_C_EN, 0);
 	gpio_set_value_cansleep(FRONT_CAM_STBY, 0);
 	gpio_set_value_cansleep(FRONT_CAM_RESET, 0);
 	
 	msleep(1);
 	
-	printk("BAFFIN_CAM_C_EN = %d , CAM_AF_EN = %d \n", gpio_get_value(ARUBA_CAM_C_EN), gpio_get_value(ARUBA_CAM_AF_EN));
-	gpio_set_value(ARUBA_CAM_A_EN, 1);  // 2 - MAIN VREG_A 2.8 ON
+	printk("BAFFIN_CAM_C_EN = %d , CAM_AF_EN = %d \n", gpio_get_value(GPIO_3M_CAM_C_EN), gpio_get_value(GPIO_3M_CAM_AF_EN));
+	gpio_set_value(GPIO_3M_CAM_A_EN, 1);  // 2 - MAIN VREG_A 2.8 ON
 	msleep(1);
 
    	gpio_set_value(FRONT_CAM_C_EN, 1);  // 3- 2M CAM CORE 1.8V ON
-   	gpio_set_value(ARUBA_CAM_AF_EN, 1);
+   	gpio_set_value(GPIO_3M_CAM_AF_EN, 1);
 	msleep(1);
-	printk("BAFFIN_CAM_A_EN = %d , BAFFIN_FRONT_CAM_C_EN = %d \n", gpio_get_value(ARUBA_CAM_A_EN), gpio_get_value(FRONT_CAM_C_EN));
+	printk("BAFFIN_CAM_A_EN = %d , BAFFIN_FRONT_CAM_C_EN = %d \n", gpio_get_value(GPIO_3M_CAM_A_EN), gpio_get_value(FRONT_CAM_C_EN));
 
     gpio_set_value(FRONT_CAM_STBY, 1);// 4 -2M CAM_C_EN Enable 
     msleep(1);
@@ -378,18 +379,18 @@ int32_t msm_sensor_power_up_baffin_duos(struct msm_sensor_ctrl_t *s_ctrl)
 
 	gpio_set_value(FRONT_CAM_STBY, 0);  // 7 - 2M CAM_C_EN Disable
 
-	gpio_set_value(ARUBA_CAM_C_EN, 1);  // 8 - 5M CAM_C_EN ON 
+	gpio_set_value(GPIO_3M_CAM_C_EN, 1);  // 8 - 5M CAM_C_EN ON 
 	msleep(10);
 
 	printk("FRONT_CAM_STBY = %d\n", gpio_get_value(FRONT_CAM_STBY));
-	printk("ARUBA_CAM_C_EN = %d\n", gpio_get_value(ARUBA_CAM_C_EN));
+	printk("GPIO_3M_CAM_C_EN = %d\n", gpio_get_value(GPIO_3M_CAM_C_EN));
 	
-	gpio_set_value(ARUBA_CAM_STBY, 1); //9 - 5M STBY ON
-	printk("ARUBA_CAM_STBY = %d\n", gpio_get_value(ARUBA_CAM_STBY));	
+	gpio_set_value(GPIO_3M_CAM_STBY, 1); //9 - 5M STBY ON
+	printk("GPIO_3M_CAM_STBY = %d\n", gpio_get_value(GPIO_3M_CAM_STBY));	
 	msleep(20);
 
-	gpio_set_value(ARUBA_CAM_RESET, 1); // 10 - 5M RESET ON
-	printk("ARUBA_CAM_RESET = %d\n", gpio_get_value(ARUBA_CAM_RESET));	
+	gpio_set_value(GPIO_3M_CAM_RESET, 1); // 10 - 5M RESET ON
+	printk("GPIO_3M_CAM_RESET = %d\n", gpio_get_value(GPIO_3M_CAM_RESET));	
 	
 	if (data->sensor_platform_info->ext_power_ctrl != NULL)
 		data->sensor_platform_info->ext_power_ctrl(1);
@@ -438,7 +439,7 @@ int32_t msm_sensor_power_down_baffin_duos(struct msm_sensor_ctrl_t *s_ctrl)
 
 	gpio_set_value_cansleep(FRONT_CAM_RESET, 0);	// turn off : 2M FRONT_CAM_RESET
 
-	gpio_set_value_cansleep(ARUBA_CAM_RESET, 0);	// turn off : 5M CAM_RESET 
+	gpio_set_value_cansleep(GPIO_3M_CAM_RESET, 0);	// turn off : 5M CAM_RESET 
 
 	msm_cam_clk_enable(&s_ctrl->sensor_i2c_client->client->dev,
 	cam_clk_info, &s_ctrl->cam_clk, ARRAY_SIZE(cam_clk_info), 0);
@@ -446,7 +447,7 @@ int32_t msm_sensor_power_down_baffin_duos(struct msm_sensor_ctrl_t *s_ctrl)
 
 	printk("#### cam_ldo_power_off_2 ####\n");
 	
-	gpio_set_value_cansleep(ARUBA_CAM_STBY, 0);		//turn off : 5M STBY
+	gpio_set_value_cansleep(GPIO_3M_CAM_STBY, 0);		//turn off : 5M STBY
 	gpio_set_value_cansleep(FRONT_CAM_C_EN, 0);		//turn off 2M CAM_C_EN
 
 	msm_camera_enable_vreg(&s_ctrl->sensor_i2c_client->client->dev,
@@ -458,19 +459,19 @@ int32_t msm_sensor_power_down_baffin_duos(struct msm_sensor_ctrl_t *s_ctrl)
 		s_ctrl->sensordata->sensor_platform_info->num_vreg,
 		s_ctrl->reg_ptr, 0);
 		
-	gpio_set_value_cansleep(ARUBA_CAM_A_EN, 0);
-	gpio_set_value_cansleep(ARUBA_CAM_C_EN, 0);
-	gpio_set_value_cansleep(ARUBA_CAM_AF_EN, 0);
+	gpio_set_value_cansleep(GPIO_3M_CAM_A_EN, 0);
+	gpio_set_value_cansleep(GPIO_3M_CAM_C_EN, 0);
+	gpio_set_value_cansleep(GPIO_3M_CAM_AF_EN, 0);
 
 	msleep(10);
 #ifdef CONFIG_S5K4ECGX
-	gpio_free(ARUBA_CAM_C_EN);
-	gpio_free(ARUBA_CAM_AF_EN);
+	gpio_free(GPIO_3M_CAM_C_EN);
+	gpio_free(GPIO_3M_CAM_AF_EN);
 #endif
 	
-	gpio_free(ARUBA_CAM_A_EN);
-	gpio_free(ARUBA_CAM_STBY);
-	gpio_free(ARUBA_CAM_RESET);
+	gpio_free(GPIO_3M_CAM_A_EN);
+	gpio_free(GPIO_3M_CAM_STBY);
+	gpio_free(GPIO_3M_CAM_RESET);
 	gpio_free(FRONT_CAM_RESET);
 	gpio_free(FRONT_CAM_STBY);
 	gpio_free(FRONT_CAM_C_EN);	
@@ -479,12 +480,12 @@ int32_t msm_sensor_power_down_baffin_duos(struct msm_sensor_ctrl_t *s_ctrl)
 
 
 
-#if (defined (CONFIG_MACH_ARUBA_OPEN) && !defined(CONFIG_MACH_KYLEPLUS_OPEN)) || defined (CONFIG_MACH_ARUBASLIM_OPEN)
+#if (defined (CONFIG_MACH_GPIO_3M_OPEN) && !defined(CONFIG_MACH_KYLEPLUS_OPEN)) || defined (CONFIG_MACH_ARUBASLIM_OPEN)
 static struct msm_cam_clk_info cam_clk_info[] = {
 	{"cam_m_clk", MSM_SENSOR_MCLK_24HZ},
 };
 
-int32_t msm_sensor_power_up_aruba_open(struct msm_sensor_ctrl_t *s_ctrl)
+int32_t msm_sensor_power_up_jena_open(struct msm_sensor_ctrl_t *s_ctrl)
 {
 	int32_t rc = 0;
 	struct msm_camera_sensor_info *data = s_ctrl->sensordata;
@@ -505,26 +506,26 @@ int32_t msm_sensor_power_up_aruba_open(struct msm_sensor_ctrl_t *s_ctrl)
 		goto config_vreg_failed;
 	}
 
-	rc = gpio_request(ARUBA_CAM_STBY, "aruba_stby");
-	rc = gpio_request(ARUBA_CAM_RESET, "aruba_reset");
-	rc = gpio_request(ARUBA_CAM_IO_EN, "aruba_a_en");
-	rc = gpio_request(ARUBA_CAM_C_EN, "aruba_c_en");
-	rc = gpio_request(ARUBA_CAM_AF_EN, "aruba_af_en");
+	rc = gpio_request(GPIO_3M_CAM_STBY, "jena_stby");
+	rc = gpio_request(GPIO_3M_CAM_RESET, "jena_reset");
+	rc = gpio_request(GPIO_3M_CAM_IO_EN, "jena_a_en");
+	rc = gpio_request(GPIO_3M_CAM_C_EN, "jena_c_en");
+	rc = gpio_request(GPIO_3M_CAM_AF_EN, "jena_af_en");
 
-	gpio_direction_output(ARUBA_CAM_STBY, 0);
-	gpio_direction_output(ARUBA_CAM_RESET, 0);
-	gpio_direction_output(ARUBA_CAM_IO_EN, 0);
-	gpio_direction_output(ARUBA_CAM_C_EN, 0);
-	gpio_direction_output(ARUBA_CAM_AF_EN, 0);
+	gpio_direction_output(GPIO_3M_CAM_STBY, 0);
+	gpio_direction_output(GPIO_3M_CAM_RESET, 0);
+	gpio_direction_output(GPIO_3M_CAM_IO_EN, 0);
+	gpio_direction_output(GPIO_3M_CAM_C_EN, 0);
+	gpio_direction_output(GPIO_3M_CAM_AF_EN, 0);
 
-	gpio_set_value_cansleep(ARUBA_CAM_STBY, 0);		//STBYN :: MSM_GPIO 96
-	gpio_set_value_cansleep(ARUBA_CAM_RESET, 0);	//RSTN :: MSM_GPIO 85
-	gpio_set_value_cansleep(ARUBA_CAM_IO_EN, 0);
-	gpio_set_value_cansleep(ARUBA_CAM_C_EN, 0);
+	gpio_set_value_cansleep(GPIO_3M_CAM_STBY, 0);		//STBYN :: MSM_GPIO 96
+	gpio_set_value_cansleep(GPIO_3M_CAM_RESET, 0);	//RSTN :: MSM_GPIO 85
+	gpio_set_value_cansleep(GPIO_3M_CAM_IO_EN, 0);
+	gpio_set_value_cansleep(GPIO_3M_CAM_C_EN, 0);
 
 	msleep(1);
-	gpio_set_value(ARUBA_CAM_C_EN, 1);
-	gpio_set_value(ARUBA_CAM_AF_EN, 1);
+	gpio_set_value(GPIO_3M_CAM_C_EN, 1);
+	gpio_set_value(GPIO_3M_CAM_AF_EN, 1);
 	msleep(1);
 
 	rc = msm_camera_enable_vreg(&s_ctrl->sensor_i2c_client->client->dev,
@@ -536,10 +537,10 @@ int32_t msm_sensor_power_up_aruba_open(struct msm_sensor_ctrl_t *s_ctrl)
 		goto enable_vreg_failed;
 	}
 	msleep(1);
-	gpio_set_value(ARUBA_CAM_IO_EN, 1);
+	gpio_set_value(GPIO_3M_CAM_IO_EN, 1);
 
 //	gpio_tlmm_config(GPIO_CFG(15, 1, 1, 1, 3, 1),GPIO_CFG_ENABLE);
-	gpio_tlmm_config(GPIO_CFG(ARUBA_CAM_FLASH_SET, 0, GPIO_CFG_OUTPUT, GPIO_CFG_PULL_DOWN, GPIO_CFG_2MA), GPIO_CFG_ENABLE);
+	gpio_tlmm_config(GPIO_CFG(GPIO_3M_CAM_FLASH_SET, 0, GPIO_CFG_OUTPUT, GPIO_CFG_PULL_DOWN, GPIO_CFG_2MA), GPIO_CFG_ENABLE);
 
 	if (s_ctrl->clk_rate != 0)
 		cam_clk_info->clk_rate = s_ctrl->clk_rate;
@@ -552,9 +553,9 @@ int32_t msm_sensor_power_up_aruba_open(struct msm_sensor_ctrl_t *s_ctrl)
 	}
 
 	msleep(1);
-	gpio_set_value_cansleep(ARUBA_CAM_STBY, 1);		//STBYN :: MSM_GPIO 96
+	gpio_set_value_cansleep(GPIO_3M_CAM_STBY, 1);		//STBYN :: MSM_GPIO 96
 	msleep(1);
-	gpio_set_value_cansleep(ARUBA_CAM_RESET, 1);	//RSTN :: MSM_GPIO 85
+	gpio_set_value_cansleep(GPIO_3M_CAM_RESET, 1);	//RSTN :: MSM_GPIO 85
 	msleep(1);
 	usleep_range(1000, 2000);
 
@@ -587,7 +588,7 @@ request_gpio_failed:
 	return rc;
 }
 
-int32_t msm_sensor_power_down_aruba_open(struct msm_sensor_ctrl_t *s_ctrl)
+int32_t msm_sensor_power_down_jena_open(struct msm_sensor_ctrl_t *s_ctrl)
 {
 	struct msm_camera_sensor_info *data = s_ctrl->sensordata;
 	CDBG("%s\n", __func__);
@@ -602,21 +603,21 @@ int32_t msm_sensor_power_down_aruba_open(struct msm_sensor_ctrl_t *s_ctrl)
 
 	cam_flash_off(FLASH_MODE_CAMERA);	//kk0704.park :: Flash Off when Power down
 
-	gpio_set_value_cansleep(ARUBA_CAM_RESET, 0);	//RSTN :: MSM_GPIO 85
+	gpio_set_value_cansleep(GPIO_3M_CAM_RESET, 0);	//RSTN :: MSM_GPIO 85
 	msm_cam_clk_enable(&s_ctrl->sensor_i2c_client->client->dev,
 		cam_clk_info, &s_ctrl->cam_clk, ARRAY_SIZE(cam_clk_info), 0);
 	msleep(1);
-	gpio_set_value_cansleep(ARUBA_CAM_STBY, 0);		//STBYN :: MSM_GPIO 96
-	gpio_set_value_cansleep(ARUBA_CAM_IO_EN, 0);
-	gpio_set_value_cansleep(ARUBA_CAM_C_EN, 0);
-	gpio_set_value_cansleep(ARUBA_CAM_AF_EN, 0);
+	gpio_set_value_cansleep(GPIO_3M_CAM_STBY, 0);		//STBYN :: MSM_GPIO 96
+	gpio_set_value_cansleep(GPIO_3M_CAM_IO_EN, 0);
+	gpio_set_value_cansleep(GPIO_3M_CAM_C_EN, 0);
+	gpio_set_value_cansleep(GPIO_3M_CAM_AF_EN, 0);
 	msleep(10);
 
-	gpio_free(ARUBA_CAM_C_EN);
-	gpio_free(ARUBA_CAM_AF_EN);
-	gpio_free(ARUBA_CAM_IO_EN);
-	gpio_free(ARUBA_CAM_STBY);
-	gpio_free(ARUBA_CAM_RESET);
+	gpio_free(GPIO_3M_CAM_C_EN);
+	gpio_free(GPIO_3M_CAM_AF_EN);
+	gpio_free(GPIO_3M_CAM_IO_EN);
+	gpio_free(GPIO_3M_CAM_STBY);
+	gpio_free(GPIO_3M_CAM_RESET);
 	msm_camera_enable_vreg(&s_ctrl->sensor_i2c_client->client->dev,
 		s_ctrl->sensordata->sensor_platform_info->cam_vreg,
 		s_ctrl->sensordata->sensor_platform_info->num_vreg,
@@ -629,6 +630,4 @@ int32_t msm_sensor_power_down_aruba_open(struct msm_sensor_ctrl_t *s_ctrl)
 	return 0;
 }
 
-#endif //(CONFIG_MACH_ARUBA_OPEN) && !(CONFIG_MACH_KYLEPLUS_OPEN)
-
-
+#endif //(CONFIG_MACH_GPIO_3M_OPEN) && !(CONFIG_MACH_KYLEPLUS_OPEN)
