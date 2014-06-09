a75dff7402b57b3c95a4d057683e6a14c859a6d9
Erik Castricum <erikcas1972@gmail.com>
2014-06-09 21:10:47 +0200

    Revert "zram: avoid invalid memory access in zram_exit()"
    This reverts commit 5666046c5717b44bf0380c1f61a41ba66d73217b.

--- drivers/staging/zram/zram_drv.c
+++ drivers/staging/zram/zram_drv.c
@@ -975,11 +975,12 @@ static void __exit zram_exit(void)
 	for (i = 0; i < num_devices; i++) {
 		zram = &zram_devices[i];
 
-		get_disk(zram->disk);
 		destroy_device(zram);
-		if (zram->init_done)
-			zram_reset_device(zram);
-		put_disk(zram->disk);
+		/*
+		 * Shouldn't access zram->disk after destroy_device
+		 * because destroy_device already released zram->disk.
+		 */
+		zram_reset_device(zram, false);
 	}
 
 	unregister_blkdev(zram_major, "zram");
