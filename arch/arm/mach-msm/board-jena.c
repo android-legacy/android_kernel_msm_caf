8d833858e60eef3c46ced6c6de77efb5392e9fbf
Erik Castricum <erikcas1972@gmail.com>
2014-06-22 11:35:23 +0200

    board-jena: enable msm: pm2: Add CPR specific function callbacks to PM driver
    cherry-picked from 0ac89b4f4329132349e1930c737eaa09ffd2f7cc
    
    Change-Id: I62aa245ea8856cff9bda0708de1f57f5dcca12bb

--- arch/arm/mach-msm/board-jena.c
+++ arch/arm/mach-msm/board-jena.c
@@ -4370,6 +4370,7 @@ static void __init msm7x27a_pm_init(void)
 				ARRAY_SIZE(msm8625_pm_data));
 		BUG_ON(msm_pm_boot_init(&msm_pm_8625_boot_pdata));
 		msm8x25_spm_device_init();
+		msm_pm_register_cpr_ops();
 	} else {
 		msm_pm_set_platform_data(msm7x27a_pm_data,
 				ARRAY_SIZE(msm7x27a_pm_data));
